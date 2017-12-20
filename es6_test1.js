fa7d5ee0eb9bf361467a87f6457fcfb5b1e2400a
Alpha <liushahedi@163.com>
2017-12-20 17:28:50 +0800

    test

---
+++ es6_test1.js
@@ -0,0 +1,17 @@
+// {
+// 	let a = 10;
+// 	var b = 1;
+// }
+
+// // console.log(a)
+// console.log(b)
+if (true){
+	tmp = 'abc';
+	console.log(tmp);
+
+	let tmp;
+	console.log(tmp);
+	tmp = 123;
+	console.log(tmp)
+
+}
